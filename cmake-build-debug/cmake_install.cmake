bcabee05e159d8c601993f2558c980770ba789b5
eslam <91751277+eslammohamedtolba@users.noreply.github.com>
2022-10-28 01:43:38 +0200

    eslam added some changes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/hp/Downloads/materials/oop/assignments/assignment2/task1-BigReal-program
+# Install script for directory: C:/Users/hp/Downloads/materials/oop/assignments/assignment2/BigReal-program
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/hp/Downloads/materials/oop/assignments/assignment2/task1-BigReal-program/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/hp/Downloads/materials/oop/assignments/assignment2/BigReal-program/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
